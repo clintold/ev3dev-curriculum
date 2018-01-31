b411efd6bf404aaa7211a51b90850f5451ed8f8b
mazanyap <Mazanayap@rose-hulman.edu>
2018-01-31 09:05:14 -0500

    yeehaw

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -18,6 +18,9 @@ import time
 
 class Snatch3r(object):
     """Commands for the Snatch3r robot that might be useful in many different programs."""
-    
-    # TODO: Implement the Snatch3r class as needed when working the sandox exercises
-    # (and delete these comments)
+
+    def drive_inches(self, inches_target, speed_deg_per_second):
+        print()
+
+
+
