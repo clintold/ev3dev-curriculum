b32989e1b524b1f4f163140c7a7578cd43dc3743
epsteirj <epsteirj@rose-hulman.edu>
2018-02-01 09:38:59 -0500

    Set folder name

--- sandbox/epsteirj/digital_inputs/m1_button_states_with_leds.py
+++ sandbox/epsteirj/digital_inputs/m1_button_states_with_leds.py
@@ -66,12 +66,12 @@ def main():
             ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
             ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
         if btn.down:
+            current_color_index += 1
             if current_color_index==4:
                 current_color_index=0
-            current_color_index+=1
             ev3.Leds.set_color(ev3.Leds.LEFT, led_colors[current_color_index])
             ev3.Leds.set_color(ev3.Leds.RIGHT, led_colors[current_color_index])
-        while btn.down==True:
+        while btn.down:
             time.sleep(0.01)
         # TODO: 3. Implement the left, right, and up buttons as follows:
         #    When the up button is being pressed:
