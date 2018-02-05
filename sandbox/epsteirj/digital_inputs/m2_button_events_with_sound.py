5190562bd7bf4fcab229119132415cb7c6e3a90e
epsteirj <epsteirj@rose-hulman.edu>
2018-02-05 08:52:13 -0500

    I'll work on digital inputs tonight if that's fine. If you want to work together I'll prob be in room 221 most of tonight so just come whenever. I think im done with all but testing for 1-4 in epsteirj

--- sandbox/epsteirj/digital_inputs/m2_button_events_with_sound.py
+++ sandbox/epsteirj/digital_inputs/m2_button_events_with_sound.py
@@ -55,7 +55,7 @@ def main():
     btn.on_down = handle_down_button
     btn.on_left = handle_left_button
     btn.on_right = handle_right_button
-    btn.on_backspace = lambda state: handle_shutdown()
+    btn.on_backspace = lambda state: handle_shutdown(state, dc)
 
     # TODO: 5. Note #4 is lower (this is TO DO #5 which you should do after #4).
     # Add a lambda callback for on_backspace.  The syntax of lambda is:
