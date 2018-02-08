e0edf53a4f944e91ae9de52c5eada20ade1e0bf0
Luke Clinton <clintold@rose-hulman.edu>
2018-02-08 09:32:00 -0500

    NAh

--- sandbox/clintold/analog_sensors/ir_sensor/m2_beacon_seeking.py
+++ sandbox/clintold/analog_sensors/ir_sensor/m2_beacon_seeking.py
@@ -9,8 +9,8 @@ function gets the robot to that location it will stop the robot and return.  Wit
 prompted if they want to find the beacon again (presumably you move it first) or quit.
 
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Luke Clinton.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 import traceback
 
 import ev3dev.ev3 as ev3
@@ -56,7 +56,11 @@ def seek_beacon(robot):
       :rtype: bool
     """
 
-    # TODO: 2. Create a BeaconSeeker object on channel 1.
+    # Done 2. Create a BeaconSeeker object on channel 1.
+    ir_sensor = ev3.InfraredSensor()
+    assert ir_sensor
+    beacon_seeker = ev3.BeaconSeeker(channel=1)
+    assert beacon_seeker
 
     forward_speed = 300
     turn_speed = 100
@@ -64,9 +68,9 @@ def seek_beacon(robot):
     while not robot.touch_sensor.is_pressed:
         # The touch sensor can be used to abort the attempt (sometimes handy during testing)
 
-        # TODO: 3. Use the beacon_seeker object to get the current heading and distance.
-        current_heading = 0  # use the beacon_seeker heading
-        current_distance = 0  # use the beacon_seeker distance
+        # Done: 3. Use the beacon_seeker object to get the current heading and distance.
+        current_heading = beacon_seeker.heading  # use the beacon_seeker heading
+        current_distance = beacon_seeker.distance  # use the beacon_seeker distance
         if current_distance == -128:
             # If the IR Remote is not found just sit idle for this program until it is moved.
             print("IR Remote not found. Distance is -128")
@@ -91,6 +95,8 @@ def seek_beacon(robot):
             # Here is some code to help get you started
             if math.fabs(current_heading) < 2:
                 # Close enough of a heading to move forward
+                if current_distance == 0:
+                    return
                 print("On the right heading. Distance: ", current_distance)
                 # You add more!
 
