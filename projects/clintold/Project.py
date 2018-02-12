196997e27b92e9ba333bd5b690bc3da201d74055
Luke Clinton <clintold@rose-hulman.edu>
2018-02-12 09:45:25 -0500

    ??

---
+++ projects/clintold/Project.py
@@ -0,0 +1,3 @@
+'''This is the final project for Lucas Clinton in the Winter quarter 2017-2918 class of CSSE120.
+  All barrowed code belongs to the individual writers. MAny of thos writers are in the CS and ECE departments.
+  Chief writer of borrowed code is David Fisher'''
