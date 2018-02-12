ca9eb4ed4fb7bb431dbccca1459de02b6d9f87f5
epsteirj <epsteirj@rose-hulman.edu>
2018-02-12 08:54:14 -0500

    I'll

--- sandbox/epsteirj/analog_sensors/color_sensor/m2_follow_a_line.py
+++ sandbox/epsteirj/analog_sensors/color_sensor/m2_follow_a_line.py
@@ -84,7 +84,7 @@ def follow_the_line(robot, white_level, black_level):
             current_color = robot.color_sensor.reflected_light_intensity
             time.sleep(.01)
         while current_color>=(white_level-10):
-            robot.drive_until_otherwise(300,200)
+            robot.drive_until_otherwise(300,0)
             current_color = robot.color_sensor.reflected_light_intensity
             time.sleep(.01)
     robot.stop()
