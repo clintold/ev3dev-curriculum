e571749023fab281cfa5a1d61325938486441678
epsteirj <epsteirj@rose-hulman.edu>
2018-02-14 09:46:20 -0500

    I'll

--- projects/epsteirj/__init__.py
+++ projects/epsteirj/__init__.py
@@ -79,11 +79,14 @@ def display_code(code,digits):
 
     for k in range(digits):
         box_label = ttk.Label(main_frame, text=" ")
-        box_label.grid(row=0, column=k)
+        box_label.grid(row=1, column=k)
         box = ttk.Entry(main_frame, width=4)
         box.insert(0, "0")
-        box.grid(row=1, column=k)
+        box.grid(row=2, column=k)
 
+    return_button = ttk.Button(main_frame, text="enter")
+    return_button.grid(row=4, column=(digits//2))
+    return_button['command'] = (lambda: check_code(mqtt_client, False))
 
 
 
